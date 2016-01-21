6e322692355878af994ce9d09ae8e90b98c4e0b5
Daniel Leivers <dan.leivers@somoglobal.com>
2016-01-21 15:06:09 +0000

    Added test harness

--- ATHeartRate/ATHeartRate/ViewController.m
+++ ATHeartRate/ATHeartRate/ViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "ViewController.h"
+#import "HeartRateDetectionModel.h"
 
-@interface ViewController ()
+@interface ViewController () <HeartRateDetectionModelDelegate>
+
+@property (strong, nonatomic) HeartRateDetectionModel *heartRateModel;
 
 @end
 
@@ -19,9 +22,35 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    
+    self.heartRateModel = [[HeartRateDetectionModel alloc] init];
+    [self.heartRateModel setDelegate:self];
+    [self.heartRateModel startDetection];
+}
+
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - HeartRateDetectionModelDelegate
+
+- (void)heartRateStart {
+    NSLog(@"Heart rate stop");
+}
+
+- (void)heartRateUpdate:(int)bpm atTime:(int)seconds {
+    NSLog(@"Heart update: %d", bpm);
+}
+
+- (void)heartRateEnd {
+    NSLog(@"Heart rate end");
+}
+
+- (void)heartRateError:(NSError *)error {
+    NSLog(@"Heart rate error: %@", error);
 }
 
 @end
